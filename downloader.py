0bc74ef4f5c23f88baa0c88c60a7b3add05cf32d
Pruthvi7767 <prathvirajkhose@gmail.com>
2026-03-11 14:59:12 +0530

    fix downloader format issue

--- downloader.py
+++ downloader.py
@@ -1,54 +1,40 @@
 import yt_dlp
 import os
-import traceback
 
 DOWNLOAD_DIR = "/tmp/downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 def download_video(url):
+    try:
 
-    format_strategies = [
-        "bestvideo+bestaudio/best",
-        "bestvideo*+bestaudio/best",
-        "best",
-        "bv*+ba/b",
-        "b"
-    ]
+        ydl_opts = {
+            "noplaylist": True,
+            "quiet": False,
+            "cookiefile": "cookies.txt",
+            "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
+            "force_ipv4": True,
 
-    for fmt in format_strategies:
-        try:
-
-            ydl_opts = {
-                "format": fmt,
-                "noplaylist": True,
-                "quiet": True,
-                "force_ipv4": True,
-                "cookiefile": "cookies.txt",
-                "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
-
-                "extractor_args": {
-                    "youtube": {
-                        "player_client": ["android"]
-                    }
+            "extractor_args": {
+                "youtube": {
+                    "player_client": ["android"]
                 }
             }
+        }
 
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=True)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
 
-            video_id = info["id"]
+        video_id = info["id"]
 
-            return {
-                "video_id": video_id,
-                "video_path": f"{DOWNLOAD_DIR}/{video_id}.mp4",
-                "title": info.get("title"),
-                "status": "success"
-            }
+        return {
+            "status": "success",
+            "video_id": video_id,
+            "title": info.get("title"),
+            "video_path": f"{DOWNLOAD_DIR}/{video_id}.mp4"
+        }
 
-        except Exception as e:
-            last_error = str(e)
-
-    return {
-        "status": "failed",
-        "error": last_error
-    }
+    except Exception as e:
+        return {
+            "status": "failed",
+            "error": str(e)
+        }
