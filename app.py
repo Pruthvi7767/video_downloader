3add6cb2cc3fccc49c2e2f9c6025e1d2fdd8389a
Pruthvi7767 <prathvirajkhose@gmail.com>
2026-03-12 22:01:55 +0530

    railway deploy

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from downloader import get_stream_url
-import traceback
 
 app = FastAPI()
 
-class Request(BaseModel):
+class VideoRequest(BaseModel):
     url: str
 
+
 @app.get("/")
-def health():
-    return {"status": "stream service running"}
+def home():
+    return {"status": "service running"}
+
 
 @app.post("/stream")
-def stream(req: Request):
-    try:
-        return get_stream_url(req.url)
-    except Exception as e:
-        traceback.print_exc()
-        return {"error": str(e)}
+def stream(req: VideoRequest):
+    return get_stream_url(req.url)
--- downloader.py
+++ downloader.py
@@ -1,18 +1,22 @@
 import yt_dlp
 
+
 def get_stream_url(url):
 
     ydl_opts = {
         "quiet": True,
         "skip_download": True,
-        "format": "best",
-        "force_ipv4": True
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android"]
+            }
+        }
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
 
-    formats = info.get("formats", [])
+    formats = info.get("formats")
 
     if not formats:
         raise Exception("No formats found")
