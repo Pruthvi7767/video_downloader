64266f9245d3f26a5c9d8e41739faaacaa0b7099
Pruthvi7767 <prathvirajkhose@gmail.com>
2026-03-13 14:55:19 +0530

    yt-dlp downloader service

--- app.py
+++ app.py
@@ -18,16 +18,33 @@ def home():
 def get_video_info(data: VideoRequest):
 
     try:
+
         ydl_opts = {
             "quiet": True,
+            "nocheckcertificate": True,
             "skip_download": True,
+            "ignoreerrors": True,
+            "cookiefile": "cookies.txt",
+            "format": "bestvideo+bestaudio/best",
             "writesubtitles": True,
             "writeautomaticsub": True,
             "subtitleslangs": ["en"],
+            "retries": 5,
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(data.url, download=False)
+
+        if not info:
+            raise Exception("Could not extract video information")
+
+        formats = info.get("formats", [])
+
+        stream_url = None
+        for f in formats:
+            if f.get("url"):
+                stream_url = f["url"]
+                break
 
         captions = info.get("automatic_captions", {})
         transcript_url = ""
@@ -38,7 +55,8 @@ def get_video_info(data: VideoRequest):
         return {
             "title": info.get("title"),
             "duration": info.get("duration"),
-            "transcript_url": transcript_url,
+            "stream_url": stream_url,
+            "transcript_url": transcript_url
         }
 
     except Exception as e:
