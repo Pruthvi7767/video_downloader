b2fa77f4a5b492471e04d2eda0858dc3dbf89760
Pruthvi7767 <prathvirajkhose@gmail.com>
2026-03-13 10:43:12 +0530

    fix railway port binding

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from downloader import get_stream_url
+import os
 
 app = FastAPI()
 
 class VideoRequest(BaseModel):
     url: str
 
-
 @app.get("/")
 def home():
     return {"status": "service running"}
 
-
 @app.post("/stream")
 def stream(req: VideoRequest):
     return get_stream_url(req.url)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
